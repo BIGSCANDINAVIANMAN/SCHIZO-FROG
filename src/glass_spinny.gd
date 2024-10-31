bdfb3bf37851a01fdcd0163a259f3a4b436d11a6
BIGSCANDINAVIANMAN <zhang769641@sas.edu.sg>
2024-10-31 12:20:47 +0800

    glass spinny

--- src/glass_spinny.gd
+++ src/glass_spinny.gd
@@ -1,12 +1,24 @@
 extends Area2D
 
 @export var player: Player
-var speed = 0.1
+var speed = 0.097
 
 func _physics_process(delta: float) -> void:
 	if inPlayer():
 		var additionalVelocity = (player.global_position - global_position).orthogonal() * speed
 		player.velocity += additionalVelocity
+	
+	for plate in getPlates():
+		var additionalVelocity = (plate.global_position - global_position).orthogonal() * speed
+		plate.velocity = additionalVelocity * 5
+		plate.move_and_slide()
 
 func inPlayer() -> bool:
 	return get_overlapping_bodies().has(player)
+
+func getPlates() -> Array:
+	var result = []
+	for body in get_overlapping_bodies():
+		if body.name.contains("plate"):
+			result.append(body)
+	return result
--- src/table.gd
+++ src/table.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 @export var main: Node2D
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
 		main.endGame("escaped")
