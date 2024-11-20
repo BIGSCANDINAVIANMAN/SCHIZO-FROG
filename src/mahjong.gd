20d23454c23a67b8cd8e66254dada60d8938fb72
BIGSCANDINAVIANMAN <zhang769641@sas.edu.sg>
2024-11-20 10:36:17 +0800

    FINAL SURELY SURELY

--- src/mahjong.gd
+++ src/mahjong.gd
@@ -9,5 +9,5 @@ func _ready() -> void:
 	$player.makeOverlayBig()
 	await get_tree().create_timer(3).timeout
 	await get_tree().create_tween().tween_property($CanvasLayer/schizoLabel, "modulate", Color(1, 1, 1, 0), 0.5).finished
-	await get_tree().create_timer(47).timeout
+	await get_tree().create_timer(27).timeout
 	$gates.queue_free()
